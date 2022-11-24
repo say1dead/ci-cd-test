3d9b1a416aa4f37a06bd318db0c739cc6e80b04f
say1dead <107905872+say1dead@users.noreply.github.com>
2022-11-24 21:01:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,77 @@ def log(x, base):
         return value
     else:
         return None
+    
+def factorial(x):
+    for i in range(1, x):
+        x *= i
+    return x
 
-k = int(input("В каком модуле хотите выполнять вычисления:\n1. Простые вычисление.\nВаш выбор - "))
+
+def sin(x):
+    x *= 3.1415926 / 180
+    value = x
+    sign = -1
+    for i in range(3, 100, 2):
+        value += (x ** i / factorial(i) * sign)
+        sign *= -1
+    return round(value, 4)
+
+
+def cos(x):
+    x *= 3.1415926 / 180
+    value = 1
+    sign = -1
+    for i in range(2, 100, 2):
+        value += (x ** i / factorial(i) * sign)
+        sign *= -1
+    return round(value, 4)
+
+
+def tg(x):
+    if not cos(x) == 0:
+        return round(sin(x) / cos(x), 4)
+    else:
+        return None
+
+
+def ctg(x):
+    if not sin(x) == 0:
+        return round(cos(x) / sin(x), 4)
+    else:
+        return None
+
+
+#  + 429*x**15/30720
+def arcsin(x):
+    if -1 < x < 1:
+        x = x + x**3/6 + 3*x**5/40 + 5*x**7/112 + 35*x**9/1152 + 81*x**11/2816 + 231*x**13/13312 + 429*x**15/30720
+        return x
+    elif x == 1 or x == -1:
+        return x * 1.57079632679
+    else:
+        return None
+
+def arccos(x):
+    if -1 <= x < 1:
+        x = 3.141592653589793238/2 - arcsin(x)
+        return x
+    elif x == 1:
+        return 0
+    else:
+        return None
+
+
+def arctg(x):
+    x = arcsin(x / ((1 + x ** 2) ** 0.5))
+    return x
+
+
+def arcctg(x):
+    x = arccos(x / ((1 + x ** 2) ** 0.5))
+    return x
+
+k = int(input("В каком модуле хотите выполнять вычисления:\n1. Простые вычисление.\2. Тригонометрия.\nВаш выбор - "))
 print("---------------\n")
 if k == 1:
     print("---------------\n")
@@ -58,7 +127,42 @@ if k == 1:
         b = int(input("Введите число логарифма - "))
         print("---------------\n")
         print("Результат - ", f"{log(b, a):.3f}")
-
+if k == 2:
+    k = int(input("Что хотим посчитать:\n1. Синус (sin).\n2. Косинус (cos).\n3. Тангенс (tg).\n4. Котангенс (ctg)."
+                  "\n5. Арксинус (arcsin).\n6. Арккосинус (arccos).\n7. Арктангенс (arctg).\n8. Арккотангенс (arcctg).\nВаш выбор - "))
+    print("---------------\n")
+    if k == 1:
+        x = float(input("Введите угол в градусах - "))
+        print("---------------\n")
+        print("Результат - ", sin(x))
+    if k == 2:
+        x = float(input("Введите угол в градусах - "))
+        print("---------------\n")
+        print("Результат - ", cos(x))
+    if k == 3:
+        x = float(input("Введите угол в градусах - "))
+        print("---------------\n")
+        print("Результат - ", tg(x))
+    if k == 4:
+        x = float(input("Введите угол в градусах - "))
+        print("---------------\n")
+        print("Результат - ", ctg(x))
+    if k == 5:
+        x = float(input("Введите угол в градусах - "))
+        print("---------------\n")
+        print("Результат - ", arcsin(x))
+    if k == 6:
+        x = float(input("Введите угол в градусах - "))
+        print("---------------\n")
+        print("Результат - ", arccos(x))
+    if k == 7:
+        x = float(input("Введите угол в градусах - "))
+        print("---------------\n")
+        print("Результат - ", arctg(x))
+    if k == 8:
+        x = float(input("Введите угол в градусах - "))
+        print("---------------\n")
+        print("Результат - ", arcctg(x))
 
 print("Спасибо за использование нашего калькулятора")
 input()
